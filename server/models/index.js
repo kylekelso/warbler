eea1e849dd0db9ceb505dc3f2ecff0d93d771d10
Kyle Kelso <kyle.kelso@outlook.com>
2019-03-01 04:31:29 -0500

    model files added

---
+++ server/models/index.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const key = require("../config/keys");
+
+if (process.env.NODE_ENV !== "production") {
+  mongoose.set("debug", true);
+}
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.Promise = Promise;
+mongoose.connect(keys.MONGO_URI, {
+  keepAlive: true,
+  useNewUrlParser: true
+});
+
+module.exports.User = require("./User");
+module.exports.Message = require("./Post");
