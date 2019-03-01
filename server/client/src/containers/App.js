18ccf0a61b7845f688acfbfad549b62503552adc
Kyle Kelso <kyle.kelso@outlook.com>
2019-03-01 10:44:35 -0500

    added auth store files

---
+++ server/client/src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>App Home</div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ server/client/src/store/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import authReducer from "./authentication";
+
+export default combineReducers({
+  auth: authReducer
+});
