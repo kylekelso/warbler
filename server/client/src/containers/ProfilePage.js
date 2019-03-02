22477a380c90b39b6551c875f12483a407aa3227
Kyle Kelso <kyle.kelso@outlook.com>
2019-03-02 09:35:26 -0500

    server fixes and userAside functional

--- server/client/src/containers/ProfilePage.js
+++ server/client/src/containers/ProfilePage.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import { setView } from "../store/actions";
 import requireAuth from "../hocs/requireAuth";
+import UserAside from "../components/UserAside";
 
 class ProfilePage extends Component {
+  async componentDidMount() {
+    await this.props.setView(this.props.match.params.username);
+  }
+
+  async componentDidUpdate(prevProps) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      await this.props.setView(this.props.match.params.username);
+    }
+  }
+
+  renderContent() {
+    return <UserAside />;
+  }
+
   render() {
     return (
       <div className="row">
-        <div>Profile Page</div>
+        <br />
+        {this.renderContent()}
       </div>
     );
   }
 }
 
-export default connect()(requireAuth(ProfilePage));
+export default connect(
+  null,
+  { setView }
+)(requireAuth(ProfilePage));
