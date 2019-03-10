a0ba7b34233085e221ec56a81242966f7b806307
Kyle Kelso <kyle.kelso@outlook.com>
2019-03-10 10:19:50 -0400

    settings page validation

--- client/src/utils/validationHelper.js
+++ client/src/utils/validationHelper.js
@@ -1,17 +1,29 @@
 import * as Yup from "yup";
 
-export const signupSchema = Yup.object().shape({
-  username: Yup.string()
+const _username = () => {
+  return Yup.string()
     .min(8, "Too short! Must be between 8 and 30 characters.")
     .max(30, "Too long! Must be between 8 and 30 characters.")
-    .required("Required field."),
-  email: Yup.string()
+    .required("Required field.");
+};
+
+const _email = () => {
+  return Yup.string()
     .email("Invalid email address.")
-    .required("Required field."),
-  password: Yup.string()
+    .required("Required field.");
+};
+
+const _password = () => {
+  return Yup.string()
     .min(8, "Too short! Must be between 8 and 30 characters.")
     .max(30, "Too long! Must be between 8 and 30 characters.")
-    .required("Required field."),
+    .required("Required field.");
+};
+
+export const signupSchema = Yup.object().shape({
+  username: _username(),
+  email: _email(),
+  password: _password(),
   passwordConfirm: Yup.string()
     .oneOf([Yup.ref("password"), null], "Password does not match.")
     .required("Required field."),
@@ -19,11 +31,16 @@ export const signupSchema = Yup.object().shape({
 });
 
 export const loginSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("Invalid email address.")
+  email: _email(),
+  password: _password()
+});
+
+export const settingsSchema = Yup.object().shape({
+  username: _username(),
+  oldPassword: _password(),
+  newPassword: _password(),
+  confirmNewPassword: Yup.string()
+    .oneOf([Yup.ref("newPassword"), null], "Password does not match.")
     .required("Required field."),
-  password: Yup.string()
-    .min(8, "Too short! Must be between 8 and 30 characters.")
-    .max(30, "Too long! Must be between 8 and 30 characters.")
-    .required("Required field.")
+  profileImgUrl: Yup.string().url("Must be a valid URL.")
 });
